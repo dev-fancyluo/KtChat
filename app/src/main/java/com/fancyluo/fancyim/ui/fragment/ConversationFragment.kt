7760ea141b91a24a1b62cbcc440f7a608499b399
fancyluo <fancyluo.dev@gmail.com>
2017-12-01 14:30:42 +0800

    初始化会话界面

--- app/src/main/java/com/fancyluo/fancyim/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/fancyluo/fancyim/ui/fragment/ConversationFragment.kt
@@ -1,7 +1,11 @@
 package com.fancyluo.fancyim.ui.fragment
 
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.fancyluo.fancyim.R
 import com.fancyluo.fancyim.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_conversation.*
 
 /**
  * fancyLuo
@@ -12,10 +16,20 @@ class ConversationFragment : BaseFragment() {
 
     override fun setupLayout() = R.layout.fragment_conversation
 
-    override fun setupTitle() = "KK-IM"
+    override fun setupTitle() = "Kt Chat"
 
     override fun showBack() = false
 
+    override fun init(view: View?, savedInstanceState: Bundle?) {
+        super.init(view, savedInstanceState)
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView() {
+        recyclerConversation.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(mContext)
+        }
+    }
 
 }
